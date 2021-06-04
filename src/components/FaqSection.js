1f1898bfde14e6200e96ee4317759325daca95ec
IsekaiGuy <isekaiguy@protonmail.com>
2021-06-04 09:28:24 +0000

    4.Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,80 @@
+import React from "react";
+
+const Faq = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How do I start?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+            repellat, ducimus repudiandae eius error non porro veniam, beatae,
+            nemo similique sit in nulla quo delectus deserunt possimus.
+            Molestiae, et iure.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+            sequi neque itaque voluptates non nesciunt, et deserunt optio, quos
+            inventore dolorem quidem quas cum. In quae quasi hic eius dolore?
+          </p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>Daily schedule</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+            repellat, ducimus repudiandae eius error non porro veniam, beatae,
+            nemo similique sit in nulla quo delectus deserunt possimus.
+            Molestiae, et iure.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+            sequi neque itaque voluptates non nesciunt, et deserunt optio, quos
+            inventore dolorem quidem quas cum. In quae quasi hic eius dolore?
+          </p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>Different payment methods</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+            repellat, ducimus repudiandae eius error non porro veniam, beatae,
+            nemo similique sit in nulla quo delectus deserunt possimus.
+            Molestiae, et iure.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+            sequi neque itaque voluptates non nesciunt, et deserunt optio, quos
+            inventore dolorem quidem quas cum. In quae quasi hic eius dolore?
+          </p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>What products do you offer?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+            repellat, ducimus repudiandae eius error non porro veniam, beatae,
+            nemo similique sit in nulla quo delectus deserunt possimus.
+            Molestiae, et iure.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+            sequi neque itaque voluptates non nesciunt, et deserunt optio, quos
+            inventore dolorem quidem quas cum. In quae quasi hic eius dolore?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Faq;
--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AboutSection from "../AboutSection";
 import ServicesSection from "../ServicesSection";
+import FaqSection from "../FaqSection";
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />;
       <ServicesSection />
+      <FaqSection />
     </>
   );
 };
