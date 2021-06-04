cbe73a951141dd2bb74092897f14a1cdef011a65
IsekaiGuy <isekaiguy@protonmail.com>
2021-06-04 08:21:08 +0000

    3.Services Section

--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -1,8 +1,14 @@
 import React from "react";
 import AboutSection from "../AboutSection";
+import ServicesSection from "../ServicesSection";
 
 const AboutUs = () => {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />;
+      <ServicesSection />
+    </>
+  );
 };
 
 export default AboutUs;
