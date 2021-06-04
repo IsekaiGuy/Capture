76af840597046f1eac6e295eac7ebc355b0702be
IsekaiGuy <isekaiguy@protonmail.com>
2021-06-04 15:33:18 +0000

    9. Routered

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from "react";
 import AboutUs from "./components/pages/AboutUs";
+import OurWork from "./components/pages/OurWork";
+import ContactUs from "./components/pages/ContactUs";
 import GlobalStyle from "./components/GlobalStyle";
 import Nav from "./components/Nav";
+//Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+
+        <Route path="/work">
+          <OurWork />
+        </Route>
+
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
